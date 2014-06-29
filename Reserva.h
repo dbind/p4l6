81325528009af46ec622b481975995c60a02154f
Aillaud <arnaud.aillaud@gmail.com>
2014-06-29 17:16:21 -0300

    Reserva.h y Reserva.cpp

---
+++ Reserva.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   Reserva.h
+ * Author: User1
+ *
+ * Created on 29 juin 2014, 15:58
+ */
+
+#ifndef RESERVA_H
+#define	RESERVA_H
+
+class Reserva 
+{
+    public:
+        Reserva(FechaHora fechaConsulta, FechaHora fechaReserva, Usuario* Medico, Usuario* Paciente);
+        virtual ~Reserva();
+    private:
+        Usuario* Medico;
+        Usuario* Paciente;
+        FechaHora fechaConsulta;
+        FechaHora fechaReserva;
+};
+
+#endif	/* RESERVA_H */
+
